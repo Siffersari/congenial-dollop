fc273c0cc66613e278f619fca6f8932eb002832f
Leewel Karani <leewelkarani@gmail.com>
2020-01-10 22:35:13 +0300

    Add switch case for action types

--- src/Reducers/CryptoReducer.js
+++ src/Reducers/CryptoReducer.js
@@ -1,3 +1,43 @@
-export default function(state = [], action) {
-  return state;
+import {
+  FETCH_COIN_DATA_SUCCESS,
+  FETCH_COIN_DATA_FAIL,
+  FETCH_COIN_DATA
+} from "../Utils/ActionTypes";
+
+const initialState = {
+  isFetching: null,
+  data: [],
+  hasError: false,
+  errorMessage: null
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_COIN_DATA:
+      return Object.assign({}, state, {
+        isFetching: true,
+        data: null,
+        hasError: false,
+        errorMessage: null
+      });
+
+    case FETCH_COIN_DATA_SUCCESS:
+      return Object.assign({}, state, {
+        isFetching: false,
+        data: action.payload,
+        hasError: false,
+        errorMessage: null
+      });
+
+    case FETCH_COIN_DATA_FAIL:
+      return Object.assign({}, state, {
+        isFetching: false,
+        data: action.payload,
+        hasError: true,
+        errorMessage: action.err
+      });
+
+    default:
+      return state;
+  }
 }
