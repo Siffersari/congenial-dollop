31df3484aa4376f9c7ae4746ef004873819cab75
Leewel Karani <leewelkarani@gmail.com>
2020-01-10 23:58:20 +0300

    Fix null data in reducer

--- src/Reducers/CryptoReducer.js
+++ src/Reducers/CryptoReducer.js
@@ -16,7 +16,7 @@ export default function(state = initialState, action) {
     case FETCH_COIN_DATA:
       return Object.assign({}, state, {
         isFetching: true,
-        data: null,
+        data: [],
         hasError: false,
         errorMessage: null
       });
