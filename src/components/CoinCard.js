5b4fe961bb26c03d18f21d9ca03b73417e0c2b0e
Leewel Karani <leewelkarani@gmail.com>
2020-01-11 20:53:45 +0300

    Add coin card component

--- src/components/CoinCard.js
+++ src/components/CoinCard.js
@@ -1,12 +1,40 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
-const CoinCard = props => {
+import { images } from "../Utils/CoinIcons";
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex"
+  },
+  image: {
+    width: 40,
+    height: 40
+  },
+  bold: {
+    fontWeight: "bold"
+  }
+});
+
+const { container, image, bold } = styles;
+
+const CoinCard = ({
+  symbol,
+  price_usd,
+  percent_change_24h,
+  coin_name,
+  percent_change_7d
+}) => {
   return (
-    <View>
-      <Text>{props.symbol}</Text>
-      <Text>{props.price_usd}</Text>
-      <Text>{props.percent_change_24h}</Text>
+    <View style={container}>
+      <Image style={styles.image} source={{ uri: images[symbol] }} />
+      <Text>{symbol}</Text>
+      <Text>{coin_name}</Text>
+      <Text>
+        {price_usd} <Text style={bold}>$</Text>
+      </Text>
+      <Text>{percent_change_24h}</Text>
+      <Text>{percent_change_7d}</Text>
     </View>
   );
 };
