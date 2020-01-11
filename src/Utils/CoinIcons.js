62b7d6d995055c0d27cc67f50849878aae73e762
Leewel Karani <leewelkarani@gmail.com>
2020-01-11 20:49:39 +0300

    Add image url

--- src/Utils/CoinIcons.js
+++ src/Utils/CoinIcons.js
@@ -1,4 +1,8 @@
 export const images = {
   BTC:
-    "https://res.cloudinary.com/da7jhtpjh/image/upload/v1508609483/bitcoin_eqld4v.png"
+    "https://res.cloudinary.com/da7jhtpjh/image/upload/v1508609483/bitcoin_eqld4v.png",
+  ETH:
+    "https://res.cloudinary.com/da7jhtpjh/image/upload/v1508609485/ethereum_nw0chu.png",
+  XRP:
+    "https://res.cloudinary.com/da7jhtpjh/image/upload/v1508609486/ripple_p0xeut.png"
 };
